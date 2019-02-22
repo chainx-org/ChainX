73382f9a78840213b014df1a0cddc85882961f5c
Aton <jincxmain@vip.163.com>
2019-02-22 16:35:10 +0800

    change cli validator_name to validator-name

--- cli/src/params.rs
+++ cli/src/params.rs
@@ -3,7 +3,7 @@ use structopt::{clap::App, StructOpt};
 
 #[derive(Clone, StructOpt, Debug)]
 pub struct ChainXParams {
-    #[structopt(long = "validator_name", value_name = "NAME")]
+    #[structopt(long = "validator-name", value_name = "NAME")]
     /// registered validator name, when give the node `--key`, must provide matching validator's unique name
     pub validator_name: Option<String>,
 }
