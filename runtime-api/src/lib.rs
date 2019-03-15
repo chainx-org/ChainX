831f98ad2899be3d6b49e81c41fcee7dc761f8bf
Qinxuan Chen <koushiro.cqx@gmail.com>
2019-03-15 11:55:22 +0800

    Migrate runtime-api to edition 2018 (#413)
    * Migrate runtime-api to edition 2018
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>
    
    * Format Cargo.toml
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>
    
    * Format Cargo.toml
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- runtime-api/src/lib.rs
+++ runtime-api/src/lib.rs
@@ -1,53 +1,35 @@
 // Copyright 2018 Chainpool.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-extern crate sr_std as rstd;
-extern crate substrate_client as client;
+use rstd::prelude::Vec;
 
-extern crate srml_support;
+use client::decl_runtime_apis;
 
-extern crate chainx_primitives;
-extern crate xr_primitives;
-
-extern crate xrml_xsupport as xsupport;
-
-extern crate xrml_xassets_assets as xassets;
-extern crate xrml_xassets_process as xprocess;
-extern crate xrml_xassets_records as xrecords;
-extern crate xrml_xdex_spot as xspot;
+use chainx_primitives::{AccountId, Balance, SessionKey, Timestamp};
 
 pub mod xassets_api {
-    use chainx_primitives::{AccountId, Balance, Timestamp};
-    use client::decl_runtime_apis;
-    use rstd::prelude::Vec;
+    use super::*;
     use xassets::{Asset, AssetType, Memo, Token};
     use xrecords::AddrStr;
     use xsupport::storage::btree_map::CodecBTreeMap;
+
     decl_runtime_apis! {
         pub trait XAssetsApi {
             fn valid_assets() -> Vec<Token>;
-
             fn all_assets() -> Vec<(Asset, bool)>;
-
             fn valid_assets_of(who: AccountId) -> Vec<(Token, CodecBTreeMap<AssetType, Balance>)>;
-
             fn withdrawal_list_of(chain: xassets::Chain) -> Vec<xrecords::RecordInfo<AccountId, Balance, Timestamp>>;
-
             fn deposit_list_of(chain: xassets::Chain) -> Vec<xrecords::RecordInfo<AccountId, Balance, Timestamp>>;
-
             fn verify_address(token: Token, addr: AddrStr, ext: Memo) -> Result<(), Vec<u8>>;
-
             fn minimal_withdrawal_value(token: Token) -> Option<Balance>;
         }
     }
 }
 
 pub mod xmining_api {
-    use chainx_primitives::AccountId;
-    use chainx_primitives::Balance;
-    use client::decl_runtime_apis;
-    use rstd::prelude::Vec;
+    use super::*;
     use xassets::Token;
+
     decl_runtime_apis! {
         pub trait XMiningApi {
             fn jackpot_accountid_for(who: AccountId) -> AccountId;
@@ -60,9 +42,9 @@ pub mod xmining_api {
 }
 
 pub mod xspot_api {
-    use chainx_primitives::Balance;
-    use client::decl_runtime_apis;
+    use super::*;
     use xassets::Token;
+
     decl_runtime_apis! {
         pub trait XSpotApi {
             fn aver_asset_price(token: Token) -> Option<Balance>;
@@ -71,8 +53,8 @@ pub mod xspot_api {
 }
 
 pub mod xfee_api {
-    use client::decl_runtime_apis;
-    use rstd::prelude::Vec;
+    use super::*;
+
     decl_runtime_apis! {
         pub trait XFeeApi {
             fn transaction_fee(call: Vec<u8>, encoded_len: u64) -> Option<u64>;
@@ -81,9 +63,8 @@ pub mod xfee_api {
 }
 
 pub mod xsession_api {
-    use chainx_primitives::{AccountId, SessionKey};
-    use client::decl_runtime_apis;
-    use rstd::prelude::Vec;
+    use super::*;
+
     decl_runtime_apis! {
         pub trait XSessionApi {
             fn pubkeys_for_validator_name(name: Vec<u8>) -> Option<(AccountId, Option<SessionKey>)>;
