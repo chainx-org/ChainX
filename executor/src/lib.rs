f514dbd5b12f5a888646d2c4d6ac99dd44a9a42a
Qinxuan Chen <koushiro.cqx@gmail.com>
2019-03-16 12:56:22 +0800

    Migrate executor to 2018 edition (#418)
    * Migrate executor to 2018 edition
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>
    
    * update wasm
    
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- executor/src/lib.rs
+++ executor/src/lib.rs
@@ -3,12 +3,9 @@
 //! A `CodeExecutor` specialisation which uses natively compiled runtime when the wasm to be
 //! executed is equivalent to the natively compiled code.
 
-extern crate chainx_runtime;
-#[macro_use]
-extern crate substrate_executor;
-#[cfg_attr(test, macro_use)]
-extern crate substrate_primitives as primitives;
-
 pub use substrate_executor::NativeExecutor;
+
+use substrate_executor::native_executor_instance;
+
 native_executor_instance!(pub Executor, chainx_runtime::api::dispatch, chainx_runtime::native_version,
   include_bytes!("../../runtime/wasm/target/wasm32-unknown-unknown/release/chainx_runtime_wasm.compact.wasm"));
