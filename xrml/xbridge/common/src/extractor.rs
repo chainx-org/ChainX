036d6c8da483fa66a7144eef5f0478a7fafc4616
Aten <jincxmain@gmail.com>
2019-05-20 14:13:56 +0800

    Update substrate and support checksum check in runtime (#623)
    * update substrate
    
    update substrate to 44d8a3955f60d7b22f356506abed205d971c791f
    
    * support checksum for parse chainx addr

--- xrml/xbridge/common/src/extractor.rs
+++ xrml/xbridge/common/src/extractor.rs
@@ -1,7 +1,13 @@
 // Copyright 2018-2019 Chainpool.
 
 use rstd::prelude::Vec;
+use primitives::traits::MaybeDebug;
+use substrate_primitives::blake2::blake2_512;
+
 use xr_primitives::{generic::b58, Name};
+#[cfg(feature = "std")]
+use xsupport::u8array_to_string;
+use xsupport::{debug, error};
 
 use crate::traits::Extractable;
 
@@ -16,37 +22,68 @@ fn split(data: &[u8]) -> Vec<Vec<u8>> {
     data.split(|x| *x == b'@').map(|d| d.to_vec()).collect()
 }
 
+const PREFIX: &[u8] = b"SS58PRE";
+
+fn ss58hash(data: &[u8]) -> [u8; 64] {
+    let mut v = PREFIX.to_vec();
+    v.extend_from_slice(data);
+    blake2_512(&v)
+}
+
 impl<AccountId> Extractable<AccountId> for Extractor<AccountId>
 where
-    AccountId: Default + AsMut<[u8]> + AsRef<[u8]>,
+    AccountId: Default + MaybeDebug + AsMut<[u8]> + AsRef<[u8]>,
 {
     /// same to `substrate/core/primitives/src/crypto.rs:trait Ss58Codec`
     fn account_info(data: &[u8], addr_type: u8) -> Option<(AccountId, Option<Name>)> {
         let v = split(data);
         if v.len() < 1 {
+            error!(
+                "[account_info]|can't parse data|data:{:}",
+                u8array_to_string(data)
+            );
             return None;
         }
 
         let op = &v[0];
         let d: Vec<u8> = match b58::from(op) {
             Ok(a) => a,
-            Err(_) => return None,
+            Err(_) => {
+                error!(
+                    "[account_info]|base58 decode error|data:{:}",
+                    u8array_to_string(data)
+                );
+                return None;
+            }
         };
 
         let mut res = AccountId::default();
         let len = res.as_ref().len();
         if d.len() != len + 3 {
+            error!("[account_info]|Invalid length. AccountId is Public, Public is 32 bytes, 1 bytes version, 2 bytes checksum|need len:{:}|len:{:}",  len + 3, d.len());
             // Invalid length. AccountId is Public, Public is 32 bytes, 1 bytes version, 2 bytes checksum
             return None;
         }
 
         // Check if the deposit address matches the current network.
-        //
         // The first byte of pubkey indicates the network type.
         if d[0] != addr_type {
+            error!(
+                "[account_info]|data type error|need:{:}|current:{:}",
+                addr_type, d[0]
+            );
             return None;
         }
         // check checksum
+        let hash = ss58hash(&d[0..len + 1]);
+        if d[len + 1..len + 3] != hash[0..2] {
+            error!(
+                "[account_info]|ss58 checksum error|calc:{:?}|provide:{:?}",
+                hash[0..2].to_vec(),
+                d[len + 1..len + 3].to_vec()
+            );
+            return None;
+        }
 
         // first byte is version
         res.as_mut().copy_from_slice(&d[1..len + 1]);
@@ -57,6 +94,10 @@ where
             None
         };
 
+        debug!(
+            "[account_info]|parse account info success!|who:{:?}|channel:{:?}",
+            res, channel_name
+        );
         Some((res, channel_name))
     }
 }
@@ -66,30 +107,65 @@ fn test_extractor() {
     use rustc_hex::FromHex;
     use substrate_primitives::crypto::UncheckedInto;
     use substrate_primitives::H256;
-    let addr: Vec<u8> = "5f423525f65a2b6eb5866a9159dee5edebb00c85e43147bf02ef1921590c4df1"
+    let addr: Vec<u8> = "f778a69d4166401048acb0f7b2625e9680609f8859c78e3d28e2549f84f0269a"
         .from_hex()
         .unwrap();
     let addr = H256::from_slice(&addr);
 
     {
+        // test for ed25519 and channel
         use substrate_primitives::ed25519::Public;
 
         let result = Extractor::<Public>::account_info(
-            "5EDc5F7Dur9stLhvD1eBwv35NxyjkzMs9oJHFBUbzYaikccb".as_bytes(),
+            "5VEW3R1T4LR3kDhYwXeeCnYrHRwRaH7E9V1KprypBe68XmY4".as_bytes(),
+            44
         );
-        assert_eq!(result, Some((addr.unchecked_into(), b"".to_vec())));
+        assert_eq!(result, Some((addr.unchecked_into(), None)));
 
         let result = Extractor::<Public>::account_info(
-            "5EDc5F7Dur9stLhvD1eBwv35NxyjkzMs9oJHFBUbzYaikccb@channel1".as_bytes(),
+            "5VEW3R1T4LR3kDhYwXeeCnYrHRwRaH7E9V1KprypBe68XmY4@channel1".as_bytes(),
+            44
         );
-        assert_eq!(result, Some((addr.unchecked_into(), b"channel1".to_vec())));
+        assert_eq!(result, Some((addr.unchecked_into(), Some(b"channel1".to_vec()))));
     }
     {
+        // test for sr25519
         use substrate_primitives::sr25519::Public;
 
         let result = Extractor::<Public>::account_info(
-            "5EDc5F7Dur9stLhvD1eBwv35NxyjkzMs9oJHFBUbzYaikccb".as_bytes(),
+            "5VEW3R1T4LR3kDhYwXeeCnYrHRwRaH7E9V1KprypBe68XmY4".as_bytes(),
+            44
         );
-        assert_eq!(result, Some((addr.unchecked_into(), b"".to_vec())));
+        assert_eq!(result, Some((addr.unchecked_into(), None)));
+    }
+    {
+        // test for checksum
+        use substrate_primitives::ed25519::Public;
+        // old checksum
+        let addr: Vec<u8> = "00308187439ac204df9e299e1e54a00000000bf348e03dad679737c91871dc53"
+            .from_hex()
+            .unwrap();
+        let addr = H256::from_slice(&addr);
+        let result = Extractor::<Public>::account_info(
+            "5C4xGQZwoNEM5mdk2U3vJbFZPr6ZKFSiqWnc9JRDcJ3w2x5D".as_bytes(),
+            42
+        );
+        assert_eq!(result, None);
+        // new checksum
+        let result = Extractor::<Public>::account_info(
+            "5C4xGQZwoNEM5mdk2U3vJbFZPr6ZKFSiqWnc9JRDcJ3w334p".as_bytes(),
+            42
+        );
+        assert_eq!(result, Some((addr.unchecked_into(), None)));
+    }
+    {
+        // test for version
+        use substrate_primitives::ed25519::Public;
+
+        let result = Extractor::<Public>::account_info(
+            "5VEW3R1T4LR3kDhYwXeeCnYrHRwRaH7E9V1KprypBe68XmY4".as_bytes(),
+            42
+        );
+        assert_eq!(result, None);
     }
 }
