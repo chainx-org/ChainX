b7f89491f5bfb7d3aca494edd69c44b1049303a8
Aten <jincxmain@gmail.com>
2019-09-03 11:48:31 +0800

    Fix/1.0.6 rpc bitcoin (#837)
    * support change bitcoin bestindex
    
    * provide rpc chainx_getExtrinsicsEventsByBlockHash

--- rpc/src/chainx/chainx_impl.rs
+++ rpc/src/chainx/chainx_impl.rs
@@ -55,6 +55,53 @@ where
         Ok(self.client.block(&self.block_id_by_number(number)?)?)
     }
 
+    fn extrinsics_events(&self, hash: Option<<Block as BlockT>::Hash>) -> Result<Value> {
+        let hash = hash.unwrap_or(self.client.info()?.chain.best_hash);
+        let state = self.state_at(Some(hash))?;
+        let number = if let Some(header) = self.client.header(&BlockId::Hash(hash))? {
+            println!("header:{:?}", header);
+            *header.number()
+        } else {
+            return Err(ErrorKind::BlockNumberErr.into());
+        };
+
+        let key = "System Events".as_bytes();
+        if let Some(events) = Self::pickout::<
+            Vec<
+                system::EventRecord<
+                    <chainx_runtime::Runtime as system::Trait>::Event,
+                    <chainx_runtime::Runtime as system::Trait>::Hash,
+                >,
+            >,
+        >(&state, &key, Hasher::TWOX128)?
+        {
+            let mut result = BTreeMap::<u32, Vec<String>>::new();
+            for event_record in events {
+                match event_record.phase {
+                    system::Phase::ApplyExtrinsic(index) => {
+                        let event = format!("{:?}", event_record.event);
+                        match result.get_mut(&index) {
+                            Some(v) => v.push(event),
+                            None => {
+                                result.insert(index, vec![event]);
+                            }
+                        }
+                    }
+                    system::Phase::Finalization => {
+                        // do nothing
+                    }
+                }
+            }
+            Ok(json!({
+                "events": result,
+                "blockHash": hash,
+                "number": number,
+            }))
+        } else {
+            Err(ErrorKind::StorageNotExistErr.into())
+        }
+    }
+
     fn next_renominate(
         &self,
         who: AccountIdForRpc,
