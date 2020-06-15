e69ef7dda6822dc67eccc78e633523b46161617b
Aten <jincxmain@gmail.com>
2020-06-15 15:24:17 +0800

    update substrate rc2 to rc3

--- runtime/build.rs
+++ runtime/build.rs
@@ -5,7 +5,7 @@ fn main() {
         .with_current_project()
         .with_wasm_builder_from_git(
             "https://github.com/paritytech/substrate.git",
-            "45b9f0a9cbf901abaa9f1fca5fe8baeed029133d",
+            "34695a85650b58bcd7d7e2a677cafc2921251d68",
         )
         .export_heap_base()
         .import_memory()
