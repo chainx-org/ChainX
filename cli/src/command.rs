22f0182fe970348c25b149bf7bc89561c0128efc
Aten <jincxmain@gmail.com>
2020-06-28 17:42:47 +0800

    Feature/xsystem (#123)
    * add xsystem crate
    
    1. add xsystem runtime moudle
    2. support paused feature
    
    * add blockedlist feature and add filter to SignedExtra
    
    1. add filter to SignedExtra to filter call when validate
    2. add blocked list feature to block account
    3. add NetworkType
    
    * add addr ss58_version support
    
    1. could set ss58_version by chain=<id> (have not finish)
    2. agree on 44 for mainnet, 42 for testnet
    
    * use a trick way to judge network type

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -50,14 +50,24 @@ impl SubstrateCli for Cli {
     }
 
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
-        Ok(match id {
-            "dev" => Box::new(chain_spec::development_config()),
-            "" | "local" => Box::new(chain_spec::local_testnet_config()),
-            path => Box::new(chain_spec::ChainSpec::from_json_file(
-                std::path::PathBuf::from(path),
-            )?),
-        })
+        // this id is from `--chain=<id>`
+        load_spec(id)
     }
+}
+
+fn load_spec(id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
+    Ok(match id {
+        "dev" => Box::new(chain_spec::development_config()),
+        "" | "local" => Box::new(chain_spec::local_testnet_config()),
+        path => {
+            let p = std::path::PathBuf::from(path);
+            if !p.exists() {
+                // TODO more better hint
+                return Err(format!("not a valid path or just allow [\"dev\", \"local\"]"))
+            }
+            Box::new(chain_spec::ChainSpec::from_json_file(p)?)
+        },
+    })
 }
 
 /// Parse and run command line arguments
@@ -67,10 +77,16 @@ pub fn run() -> sc_cli::Result<()> {
     match &cli.subcommand {
         Some(subcommand) => {
             let runner = cli.create_runner(subcommand)?;
+            let chain_spec = &runner.config().chain_spec;
+            set_default_ss58_version(chain_spec);
+
             runner.run_subcommand(subcommand, |config| Ok(new_full_start!(config).0))
         }
         None => {
             let runner = cli.create_runner(&cli.run)?;
+            let chain_spec = &runner.config().chain_spec;
+            set_default_ss58_version(chain_spec);
+
             runner.run_node(
                 service::new_light,
                 service::new_full,
@@ -78,4 +94,18 @@ pub fn run() -> sc_cli::Result<()> {
             )
         }
     }
+}
+
+fn set_default_ss58_version(spec: &Box<dyn sc_service::ChainSpec>) {
+    use chainx_runtime::NetworkType;
+    use sp_core::crypto::Ss58AddressFormat;
+    // this `id()` is from `ChainSpec::from_genesis()` second parameter
+    // todo may use a better way
+    let type_: NetworkType = if spec.id().contains("mainnet") {
+        NetworkType::Mainnet
+    } else {
+        NetworkType::Testnet
+    };
+    let ss58_version = Ss58AddressFormat::Custom(type_.addr_version());
+    sp_core::crypto::set_default_ss58_version(ss58_version);
 }
