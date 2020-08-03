b3b5e47e3803eb61bfb9cde78fd3f87420011f12
Aten <jincxmain@gmail.com>
2020-08-03 14:32:37 +0800

    update runtime and node verion (#171)
    1. node version to 1.1.1
    2. runtime-version to 8

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -83,8 +83,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-net"),
     authoring_version: 1,
-    spec_version: 7,
-    impl_version: 7,
+    spec_version: 8,
+    impl_version: 8,
     apis: RUNTIME_API_VERSIONS,
 };
 
