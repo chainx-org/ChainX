a97da7276704967cdb71659eeda9001952998ce2
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-08-08 10:49:48 +0800

    Remove =>

--- xpallets/mining/staking/src/reward/mod.rs
+++ xpallets/mining/staking/src/reward/mod.rs
@@ -35,13 +35,13 @@ impl<T: Trait> Module<T> {
         // Validator themselves can only directly gain 10%, the rest 90% is for the reward pot.
         let off_the_table = (reward.saturated_into() / 10).saturated_into();
         Self::mint(who, off_the_table);
-        debug!("[mint]=> validator({:?}): {:?}", who, off_the_table);
+        debug!("[mint]validator({:?}): {:?}", who, off_the_table);
 
         // Issue the rest 90% to validator's reward pot.
         let to_reward_pot = reward - off_the_table;
         let reward_pot = T::DetermineRewardPotAccount::reward_pot_account_for(who);
         Self::mint(&reward_pot, to_reward_pot);
-        debug!("[mint]=> reward_pot({:?}): {:?}", reward_pot, to_reward_pot);
+        debug!("[mint]reward_pot({:?}): {:?}", reward_pot, to_reward_pot);
     }
 
     /// Reward the intention and slash the validators that went offline in last session.
@@ -61,7 +61,7 @@ impl<T: Trait> Module<T> {
             let to_vesting = this_session_reward / 5.saturated_into();
             let vesting_account = Self::vesting_account();
             Self::mint(&vesting_account, to_vesting);
-            debug!("[mint]=> vesting({:?}): {:?}", vesting_account, to_vesting);
+            debug!("[mint]vesting({:?}): {:?}", vesting_account, to_vesting);
             this_session_reward - to_vesting
         } else {
             this_session_reward
