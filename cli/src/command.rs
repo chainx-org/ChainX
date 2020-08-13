7552bbd19254d6dc0e57681e54a2852009d320e6
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-08-13 17:04:18 +0800

    Add support for cli config file (#186)
    * Add support for cli config file
    
    * Update docs
    
    * Avoid allocation
    
    * Update docs
    
    * .
    
    * .
    
    * .

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -58,7 +58,10 @@ fn load_spec(id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
 
 /// Parse and run command line arguments
 pub fn run() -> sc_cli::Result<()> {
-    let cli = Cli::from_args();
+    // Workaround for https://github.com/paritytech/substrate/issues/6856
+    // Remove this once the cli config file is supported in Substrate.
+    let raw_cli_args = std::env::args().collect::<Vec<_>>();
+    let cli = Cli::from_iter(crate::config::preprocess_cli_args(raw_cli_args));
 
     if cli.log4rs {
         crate::logger::init(&cli.run.log_filters()?, &cli)?;
