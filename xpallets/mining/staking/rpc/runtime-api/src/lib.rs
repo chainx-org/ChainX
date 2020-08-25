d5aa64bd95c8b8565c0c59fe27bf2631b80caa23
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-08-25 10:11:06 +0800

    Add xstaking_getNominatorByAccount rpc

--- xpallets/mining/staking/rpc/runtime-api/src/lib.rs
+++ xpallets/mining/staking/rpc/runtime-api/src/lib.rs
@@ -5,7 +5,7 @@
 use codec::Codec;
 use sp_std::collections::btree_map::BTreeMap;
 use sp_std::prelude::*;
-use xpallet_mining_staking::{RpcNominatorLedger, ValidatorInfo};
+use xpallet_mining_staking::{NominatorInfo, RpcNominatorLedger, ValidatorInfo};
 use xpallet_support::RpcBalance;
 
 sp_api::decl_runtime_apis! {
@@ -26,5 +26,8 @@ sp_api::decl_runtime_apis! {
 
         /// Get the nomination details given the staker AccountId.
         fn nomination_details_of(who: AccountId) -> BTreeMap<AccountId, RpcNominatorLedger<RpcBalance<Balance>, BlockNumber>>;
+
+        /// Get individual nominator information given the nominator AccountId.
+        fn nominator_info_of(who: AccountId) -> NominatorInfo<RpcBalance<Balance>, BlockNumber>;
     }
 }
