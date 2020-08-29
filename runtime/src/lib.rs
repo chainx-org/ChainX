7209e1e0ee8620d02bfb2af76f981daf31f1def3
Aten <jincxmain@gmail.com>
2020-08-29 10:51:06 +0800

    add pallet_finality_tracker module

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -331,6 +331,17 @@ impl pallet_grandpa::Trait for Runtime {
 }
 
 parameter_types! {
+    pub WindowSize: BlockNumber = pallet_finality_tracker::DEFAULT_WINDOW_SIZE.into();
+    pub ReportLatency: BlockNumber = pallet_finality_tracker::DEFAULT_REPORT_LATENCY.into();
+}
+
+impl pallet_finality_tracker::Trait for Runtime {
+    type OnFinalizationStalled = ();
+    type WindowSize = WindowSize;
+    type ReportLatency = ReportLatency;
+}
+
+parameter_types! {
     pub const Offset: BlockNumber = 0;
     pub const Period: BlockNumber = 50;
     pub const DisabledValidatorsThreshold: Perbill = Perbill::from_percent(17);
@@ -943,6 +954,7 @@ construct_runtime!(
         Offences: pallet_offences::{Module, Call, Storage, Event},
         Historical: pallet_session_historical::{Module},
         Session: pallet_session::{Module, Call, Storage, Event, Config<T>},
+        FinalityTracker: pallet_finality_tracker::{Module, Call, Storage, Inherent},
         Grandpa: pallet_grandpa::{Module, Call, Storage, Config, Event},
         ImOnline: pallet_im_online::{Module, Call, Storage, Event<T>, ValidateUnsigned, Config<T>},
         AuthorityDiscovery: pallet_authority_discovery::{Module, Call, Config},
