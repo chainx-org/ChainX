be886887f7281b2ec3c2ab81adb46212702b45bc
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-11-04 21:48:10 +0800

    Polish script

--- scripts/generate_key.sh
+++ scripts/generate_key.sh
@@ -8,12 +8,29 @@ CHAIN=testnet
 
 CHAINX=../target/release/chainx
 
-print_address() {
-  echo "                       Address:" $("$CHAINX" key inspect-key "$1" | tail -n 1 | awk '{print $NF}')
+print_validator_address() {
+  address=$("$CHAINX" key inspect-key "$1" | tail -n 1 | awk '{print $NF}')
+  echo "                // $address"
 }
 
-print_pubkey() {
-  echo "                    Public key:" $("$CHAINX" key inspect-key "$1" | tail -n 3 | head -1 | awk '{print $NF}')
+print_address() {
+  address=$("$CHAINX" key inspect-key "$1" | tail -n 1 | awk '{print $NF}')
+  echo "            // $address"
+}
+
+print_validator_id() {
+  pubkey=$("$CHAINX" key inspect-key "$1" | tail -n 3 | head -1 | awk '{print $NF}')
+  echo "                hex![\"${pubkey:2}\"].into(),"
+}
+
+print_account_key() {
+  pubkey=$("$CHAINX" key inspect-key "$1" | tail -n 3 | head -1 | awk '{print $NF}')
+  echo "            hex![\"${pubkey:2}\"].into(),"
+}
+
+print_other_key() {
+  pubkey=$("$CHAINX" key inspect-key "$1" | tail -n 3 | head -1 | awk '{print $NF}')
+  echo "            hex![\"${pubkey:2}\"].unchecked_into(),"
 }
 
 for id in 1 2 3; do
@@ -21,36 +38,38 @@ for id in 1 2 3; do
   dir="keys/$id"
 
   echo "          SECRET//validator//$id:"
-  print_address "$SECRET//validator//$id"
-  print_pubkey  "$SECRET//validator//$id"
 
-  echo "         SECRET//blockauthor/$id:"
-  print_address "$SECRET//blockauthor//$id"
-  print_pubkey  "$SECRET//blockauthor//$id"
+  echo "SECRET//blockauthor/$id, SECRET//babe//$id, SECRET//grandpa//$id, SECRET//im_online//$id, SECRET//authority_discovery//$id"
+
+  echo "            ("
+  print_validator_address "$SECRET//validator//$id"
+  print_validator_id  "$SECRET//validator//$id"
+
+  echo "                b\"ValidatorId"$id"\".to_vec(),"
+  echo "            ),"
+
+  print_address      "$SECRET//blockauthor//$id"
+  print_account_key  "$SECRET//blockauthor//$id"
 
   "$CHAINX" key insert --chain=$CHAIN --key-type babe -d $dir --scheme sr25519 --suri "$SECRET//$id//babe"
-  echo "               SECRET//babe//$id:"
-  print_address "$SECRET//babe//$id"
-  print_pubkey  "$SECRET//babe//$id"
+  print_address   "$SECRET//babe//$id"
+  print_other_key "$SECRET//babe//$id"
 
   "$CHAINX" key insert --chain=$CHAIN --key-type gran -d $dir --scheme ed25519 --suri "$SECRET//$id//grandpa"
-  echo "            SECRET//grandpa//$id:"
-  print_address "$SECRET//grandpa//$id"
-  print_pubkey  "$SECRET//grandpa//$id"
+  print_address    "$SECRET//grandpa//$id"
+  print_other_key  "$SECRET//grandpa//$id"
 
   "$CHAINX" key insert --chain=$CHAIN --key-type imon -d $dir --scheme sr25519 --suri "$SECRET//$id//im_online"
-  echo "          SECRET//im_online//$id:"
-  print_address "$SECRET//im_online//$id"
-  print_pubkey  "$SECRET//im_online//$id"
+  print_address    "$SECRET//im_online//$id"
+  print_other_key  "$SECRET//im_online//$id"
 
   "$CHAINX" key insert --chain=$CHAIN --key-type audi -d $dir --scheme sr25519 --suri "$SECRET//$id//authority_discovery"
-  echo "SECRET//authority_discovery//$id:"
-  print_address "$SECRET//authority_discovery//$id"
-  print_pubkey  "$SECRET//authority_discovery//$id"
+  print_address    "$SECRET//authority_discovery//$id"
+  print_other_key  "$SECRET//authority_discovery//$id"
   echo
 done
 
 
 echo '                          Root:'
-print_pubkey "$SECRET"
 print_address "$SECRET"
+print_account_key "$SECRET"
