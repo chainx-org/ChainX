edc338881e62a22e30e2ea3f01e1f1eb0adce6c2
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-11-10 23:38:36 +0800

    Polish the script

---
+++ scripts/ci/generate_release_note.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+if __name__ == '__main__':
+    import json
+    env = json.load(open('srtool_output.json'))
+    message = f"""
+WASM runtime built with [srtool](TODO: link to docker image) using `{env['rustc']}`.
+
+chainx runtime proposal hash: `{env['prop']}`.
+    """
+    print(message)
--- scripts/release_log.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-if __name__ == '__main__':
-    import json
-    log = json.load(open('srtool_output.json'))
-    print("### build environment  \n{}  \n ### wasm info  \nchainx_runtion hash: {}  \n".format(log['rustc'], log['sha256']))
