6c2eb98dc69e82216137dfd1362d028877d45b71
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-11-10 17:51:14 +0800

    Fix runtime benchmarks building (#340)
    * Check feature runtime-benchmarks in CI
    
    * Fix runtime-benchmarks building

--- xpallets/gateway/bitcoin/src/lib.rs
+++ xpallets/gateway/bitcoin/src/lib.rs
@@ -4,7 +4,7 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "runtime-benchmarks")]
+#[cfg(any(feature = "runtime-benchmarks", test))]
 mod benchmarking;
 pub mod header;
 mod tests;
