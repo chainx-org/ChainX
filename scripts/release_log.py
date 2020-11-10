33e3de1db59999f95ff3ea23e35545bac0fb3992
Alan WANG <alan1995wang@outlook.com>
2020-11-10 18:18:37 +0800

    Add artifact upload

--- scripts/release_log.py
+++ scripts/release_log.py
@@ -2,4 +2,4 @@
 if __name__ == '__main__':
     import json
     log = json.load(open('srtool_output.json'))
-    print("### build environment  \n{}  \n ### wasm info  \nchainx_runtion hash: {}  \n", log['rustc'], log['sha256'])
+    print("### build environment  \n{}  \n ### wasm info  \nchainx_runtion hash: {}  \n".format(log['rustc'], log['sha256']))
