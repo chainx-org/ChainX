00f59e6bc3993728af8e8a68800a236a785094e9
Alan WANG <alan1995wang@outlook.com>
2020-11-11 09:48:25 +0800

    Update release log template

--- scripts/ci/generate_release_note.py
+++ scripts/ci/generate_release_note.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     import json
     env = json.load(open('srtool_output.json'))
     message = f"""
-WASM runtime built with [srtool](TODO: link to docker image) using `{env['rustc']}`.
+WASM runtime built with [srtool](https://hub.docker.com/repository/docker/chainxcn/srtools) using `{env['rustc']}`.
 
 chainx runtime proposal hash: `{env['prop']}`.
 
