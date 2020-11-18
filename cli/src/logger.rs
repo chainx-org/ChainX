1287f6379e37bcf48a2b90d52e11a50d1930a7ea
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-11-18 11:13:33 +0800

    Enable the log rotation for non-dev chain by default (#356)
    * Enable the log rotation for non-dev chain by default
    
    * Rename to enable_console_log to make it more explicit that it's a switch
    
    * .
    
    * Try cache@v2
    
    * Switch to new cache@v2 example

--- cli/src/logger.rs
+++ cli/src/logger.rs
@@ -20,25 +20,29 @@ use log4rs::{
 
 #[derive(Debug, structopt::StructOpt)]
 pub struct LoggerParams {
-    /// Enable the log4rs feature, including the log rotation function.
+    /// Disable the log rotation.
     //  Use `log4rs` as `env_logger` can't print the message into file directly.
-    #[structopt(long = "log4rs")]
-    pub log4rs: bool,
+    #[structopt(long)]
+    pub no_log_rotation: bool,
+
+    /// Print the log message to the console aside from the log file.
+    #[structopt(long)]
+    pub enable_console_log: bool,
 
     /// Specify the path of directory which will contain the log files.
     ///
     /// The directory will be created if it does not exist.
-    #[structopt(long = "log-dir", default_value = "./log")]
+    #[structopt(long, default_value = "./log")]
     pub log_dir: String,
 
     /// Specify the name of log file.
     ///
     /// The latest log file would be created at ./log/chainx.log.
-    #[structopt(long = "log-filename", default_value = "chainx.log")]
+    #[structopt(long, default_value = "chainx.log")]
     pub log_filename: String,
 
     /// Rotate the log file when it exceeds this size (in MB).
-    #[structopt(long = "log-size", default_value = "300")]
+    #[structopt(long, default_value = "300")]
     pub log_size: u64,
 
     /// The maximum number of log rorations.
@@ -46,17 +50,13 @@ pub struct LoggerParams {
     /// By default the generated log files would be like `chainx.log.0`,
     /// `chainx.log.1`, ... `chainx.log.10`. Once the number of generated log files
     /// are larger than this variable, the oldest one will be removed.
-    #[structopt(long = "log-roll-count", default_value = "10")]
+    #[structopt(long, default_value = "10")]
     pub log_roll_count: u32,
-
-    /// Print the log message to the console aside from the log file.
-    #[structopt(long = "log-console")]
-    pub log_console: bool,
 
     /// Compress the old log file to save some disk space.
     ///
     /// The compressed log file would be like `chainx.log.gz.0` by default.
-    #[structopt(long = "log-compression")]
+    #[structopt(long)]
     pub log_compression: bool,
 }
 
@@ -193,7 +193,7 @@ pub fn init(log_filters: &str, params: &LoggerParams) -> Result<(), String> {
         .build(full_log_filename, Box::new(policy))
         .map_err(|e| format!("{}", e))?;
 
-    let mut config_builder = if params.log_console {
+    let mut config_builder = if params.enable_console_log {
         let console = ConsoleAppender::builder()
             .encoder(Box::new(PatternEncoder::new(console_pattern)))
             .build();
@@ -211,7 +211,7 @@ pub fn init(log_filters: &str, params: &LoggerParams) -> Result<(), String> {
         }
     }
 
-    let root = if params.log_console {
+    let root = if params.enable_console_log {
         config::Root::builder()
             .appender("roll")
             .appender("console")
