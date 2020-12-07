0d6d94d982d37bed3cdcba017c286f70d0823976
koushiro <koushiro.cqx@gmail.com>
2020-12-07 16:03:09 +0800

    Rename some functions
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- xpallets/gateway/bitcoin/src/lib.rs
+++ xpallets/gateway/bitcoin/src/lib.rs
@@ -56,7 +56,7 @@ use xpallet_support::try_addr;
 pub use self::types::{BtcAddress, BtcParams, BtcTxVerifier, BtcWithdrawalProposal};
 pub use self::weights::WeightInfo;
 use self::{
-    trustee::{get_last_trustee_address_pair, get_trustee_address_pair},
+    trustee::{get_current_trustee_address_pair, get_last_trustee_address_pair},
     tx::remove_pending_deposit,
     types::{
         BtcDepositCache, BtcHeaderIndex, BtcHeaderInfo, BtcRelayedTx, BtcRelayedTxInfo,
@@ -435,7 +435,7 @@ impl<T: Trait> ChainT<BalanceOf<T>> for Module<T> {
             err
         })?;
 
-        match get_trustee_address_pair::<T>() {
+        match get_current_trustee_address_pair::<T>() {
             Ok((hot_addr, cold_addr)) => {
                 // do not allow withdraw from trustee address
                 if address == hot_addr || address == cold_addr {
@@ -600,15 +600,15 @@ impl<T: Trait> Module<T> {
 
         let network = Module::<T>::network_id();
         let min_deposit = Module::<T>::btc_min_deposit();
-        let current_trustee_pair = get_trustee_address_pair::<T>()?;
-        let previous_trustee_pair = get_last_trustee_address_pair::<T>().ok();
+        let current_trustee_pair = get_current_trustee_address_pair::<T>()?;
+        let last_trustee_pair = get_last_trustee_address_pair::<T>().ok();
         let state = tx::process_tx::<T>(
             tx.raw,
             prev_tx,
             network,
             min_deposit,
             current_trustee_pair,
-            previous_trustee_pair,
+            last_trustee_pair,
         );
         TxState::insert(&tx_hash, state);
         Self::deposit_event(Event::<T>::TxProcessed(tx_hash, block_hash, state));
