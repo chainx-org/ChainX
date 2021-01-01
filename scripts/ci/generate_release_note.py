a94ea36440fa970f183e1f5f84d2d6a50f7a510b
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-01-02 02:37:48 +0800

    Add upgrade priority in release note template

--- scripts/ci/generate_release_note.py
+++ scripts/ci/generate_release_note.py
@@ -4,6 +4,11 @@ if __name__ == '__main__':
     import json
     env = json.load(open('srtool_output.json'))
     message = f"""
+Upgrade priority:
+
+- [ ] **Medium** (upgrade at your earliest convenience)
+- [ ] **High** (upgrade ASAP)
+
 WASM runtime built with [srtool](https://hub.docker.com/r/chainxorg/srtool) using `{env['rustc']}`.
 
 chainx runtime proposal hash: `{env['prop']}`.
