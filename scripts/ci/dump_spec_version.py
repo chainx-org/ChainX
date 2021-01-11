7cf0a846ca994d2bb495abccc226769ed7a02e60
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-01-11 17:21:54 +0800

    .

--- scripts/ci/dump_spec_version.py
+++ scripts/ci/dump_spec_version.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os
 import json
+
 
 def main():
     #  Switch the working directory to project root directory.
@@ -15,12 +16,17 @@ def main():
     f = open("runtime/chainx/src/lib.rs")
     for line in f.readlines():
         if line.strip().startswith('spec_version'):
-            version = ([int(s) for s in line.strip()[:-1].split() if s.isdigit()])
+            version = ([
+                int(s) for s in line.strip()[:-1].split() if s.isdigit()
+            ])
             with open("spec_version.json", 'w') as outfile:
                 if not version:
-                    json.dump({ 'version': '?' }, outfile)
+                    spec_version = {'version': '?'}
                 else:
-                    json.dump({ 'version': version[0] }, outfile)
+                    spec_version = {'version': version[0]}
+                json.dump(spec_version, outfile)
+                print(spec_version)
             break
+
 
 main()
