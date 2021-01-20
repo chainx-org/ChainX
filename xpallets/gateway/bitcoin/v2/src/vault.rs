e8787ed1a18ef16c1480820cd5937553e7f7eb1e
Alan WANG <alan1995wang@outlook.com>
2021-01-20 10:00:13 +0800

    Remove useless associate type

--- xpallets/gateway/bitcoin/v2/src/vault.rs
+++ xpallets/gateway/bitcoin/v2/src/vault.rs
@@ -1,5 +1,4 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-#![allow(dead_code)]
 
 pub mod types {
     use codec::HasCompact;
@@ -26,19 +25,19 @@ pub mod types {
 
     #[derive(Encode, Decode, Default, Clone, PartialEq)]
     #[cfg_attr(feature = "std", derive(Debug))]
-    pub struct Vault<AccountId, BlockNumber, XBtcToken> {
-        // Account identifier of the Vault
+    pub struct Vault<AccountId, BlockNumber, Balance> {
+        /// Account identifier of the Vault
         pub id: AccountId,
-        // Number of XBtcToken tokens pending issue
-        pub to_be_issued_tokens: XBtcToken,
-        // Number of issued XBtcToken tokens
-        pub issued_tokens: XBtcToken,
-        // Number of XBtcToken tokens pending redeem
-        pub to_be_redeemed_tokens: XBtcToken,
-        // Bitcoin address of this Vault (P2PKH, P2SH, P2PKH, P2WSH)
+        /// Number of tokens pending issue
+        pub to_be_issued_tokens: Balance,
+        /// Number of issued tokens
+        pub issued_tokens: Balance,
+        /// Number of tokens pending redeem
+        pub to_be_redeemed_tokens: Balance,
+        /// Bitcoin address of this Vault (P2PKH, P2SH, P2PKH, P2WSH)
         pub wallet: BtcAddress,
-        // Block height until which this Vault is banned from being
-        // used for Issue, Redeem (except during automatic liquidation) and Replace .
+        /// Block height until which this Vault is banned from being
+        /// used for Issue, Redeem (except during automatic liquidation) and Replace .
         pub banned_until: Option<BlockNumber>,
         /// Current status of the vault
         pub status: VaultStatus,
@@ -62,6 +61,7 @@ pub mod types {
 }
 
 #[frame_support::pallet]
+#[allow(dead_code)]
 pub mod pallet {
     use super::types::*;
     #[cfg(feature = "std")]
@@ -79,16 +79,12 @@ pub mod pallet {
 
     use sp_runtime::DispatchResult;
 
-    pub type PCX<T> =
+    pub type BalanceOf<T> =
         <<T as Config>::PCX as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-
-    pub type XBtcToken<T> =
-        <<T as Config>::XBtcToken as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
         type PCX: ReservableCurrency<Self::AccountId>;
-        type XBtcToken: ReservableCurrency<Self::AccountId>;
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
     }
 
@@ -105,7 +101,7 @@ pub mod pallet {
         #[pallet::weight(0)]
         fn register_vault(
             origin: OriginFor<T>,
-            collateral: PCX<T>,
+            collateral: BalanceOf<T>,
             btc_address: BtcAddress,
         ) -> DispatchResultWithPostInfo {
             let sender = ensure_signed(origin)?;
@@ -149,13 +145,13 @@ pub mod pallet {
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         /// When a new vault has been registered.
-        VaultRegistered(<T as frame_system::Config>::AccountId, PCX<T>),
+        VaultRegistered(<T as frame_system::Config>::AccountId, BalanceOf<T>),
     }
 
     /// Total collateral.
     #[pallet::storage]
     #[pallet::getter(fn total_collateral)]
-    pub(super) type TotalCollateral<T: Config> = StorageValue<_, PCX<T>, ValueQuery>;
+    pub(super) type TotalCollateral<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
 
     /// Mapping account to vault struct.
     #[pallet::storage]
@@ -163,7 +159,7 @@ pub mod pallet {
         _,
         Blake2_128Concat,
         T::AccountId,
-        Vault<T::AccountId, T::BlockNumber, XBtcToken<T>>,
+        Vault<T::AccountId, T::BlockNumber, BalanceOf<T>>,
     >;
 
     /// Mapping btc address to vault id.
@@ -173,7 +169,7 @@ pub mod pallet {
     /// Lower bound for registering vault or withdrawing collateral.
     #[pallet::storage]
     #[pallet::getter(fn minimium_vault_collateral)]
-    pub(super) type MinimiumVaultCollateral<T: Config> = StorageValue<_, PCX<T>, ValueQuery>;
+    pub(super) type MinimiumVaultCollateral<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
 
     #[pallet::genesis_config]
     #[derive(Default)]
@@ -184,28 +180,28 @@ pub mod pallet {
     #[pallet::genesis_build]
     impl<T: Config> GenesisBuild<T> for GenesisConfig {
         fn build(&self) {
-            let pcx: PCX<T> = self.minimium_vault_collateral.into();
+            let pcx: BalanceOf<T> = self.minimium_vault_collateral.into();
             <MinimiumVaultCollateral<T>>::put(pcx);
         }
     }
     impl<T: Config> Pallet<T> {
         /// Lock collateral
         #[inline]
-        fn lock_collateral(sender: &T::AccountId, amount: PCX<T>) -> DispatchResult {
+        fn lock_collateral(sender: &T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
             T::PCX::reserve(sender, amount).map_err(|_| Error::<T>::InsufficientFunds)?;
             Ok(())
         }
 
         /// increase total collateral
         #[inline]
-        fn increase_total_collateral(amount: PCX<T>) {
+        fn increase_total_collateral(amount: BalanceOf<T>) {
             <TotalCollateral<T>>::mutate(|c| *c += amount);
         }
 
         #[inline]
         fn insert_vault(
             sender: &T::AccountId,
-            vault: Vault<T::AccountId, T::BlockNumber, XBtcToken<T>>,
+            vault: Vault<T::AccountId, T::BlockNumber, BalanceOf<T>>,
         ) {
             <Vaults<T>>::insert(sender, vault);
         }
