ebcdc33478a0bd855c517d204a89593d95b1e335
Alan WANG <alan1995wang@outlook.com>
2021-01-20 16:38:42 +0800

    Rename test file

---
+++ xpallets/gateway/bitcoin/v2/src/tests.rs
@@ -0,0 +1,34 @@
+use frame_support::{assert_err, assert_ok};
+
+use super::mock::{ExtBuilder, Origin, Test};
+use super::vault::pallet as vault;
+
+#[test]
+fn test_register_vault() {
+    ExtBuilder::build(100).execute_with(|| {
+        let register_vault = |id, collateral, addr: &str| {
+            vault::Pallet::<Test>::register_vault(
+                Origin::signed(id),
+                collateral,
+                addr.as_bytes().to_vec(),
+            )
+        };
+        assert_err!(
+            register_vault(1, 10000, "test"),
+            vault::Error::<Test>::InsufficientFunds
+        );
+        assert_err!(
+            register_vault(1, 10, "test"),
+            vault::Error::<Test>::InsufficientVaultCollateralAmount
+        );
+        assert_ok!(register_vault(1, 200, "test"));
+        assert_err!(
+            register_vault(1, 200, "testuu"),
+            vault::Error::<Test>::VaultAlreadyRegistered
+        );
+        assert_err!(
+            register_vault(2, 200, "test"),
+            vault::Error::<Test>::BtcAddressOccupied
+        );
+    })
+}
