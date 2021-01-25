26d1e63f1177ef1a43fe482de068fd8e641dea88
hacpy <hacpy.lbx@gmail.com>
2021-01-25 11:52:05 +0800

    fix tests

--- xpallets/gateway/bitcoin/offchain/src/tests.rs
+++ xpallets/gateway/bitcoin/offchain/src/tests.rs
@@ -86,7 +86,7 @@ fn fetch_transaction() {
 
     state.write().expect_request(testing::PendingRequest {
         method: "GET".into(),
-        uri: "https://blockstream.info/api/tx/4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b/hex".into(),
+        uri: "https://blockstream.info/api/tx/4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b/raw".into(),
         response: Some(hex::decode("01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000").unwrap()),
         sent: true,
         ..Default::default()
@@ -128,8 +128,7 @@ fn send_raw_transaction() {
 
 #[test]
 fn parse_send_raw_tx_err() {
-    let resp_body =
-        r#"sendrawtransaction RPC error: {"code":-25,"message":"bad-txns-inputs-missingorspent"}"#;
+    let resp_body = r#"send raw transaction RPC error: {"code":-25,"message":"bad-txns-inputs-missingorspent"}"#;
     let err = XGatewayBitcoinRelay::parse_send_raw_tx_error(resp_body).unwrap();
     assert_eq!(err.code, -25);
     assert_eq!(err.message, "bad-txns-inputs-missingorspent");
