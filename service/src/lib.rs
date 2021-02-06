1fbbac4f567d7895c7d81bcfc5b9c9a4220c1e9b
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-02-06 09:51:56 +0800

    Update Substrate to f14488dfca012659297d2b4676fab91c179095dd

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -83,6 +83,7 @@ where
 
     let transaction_pool = sc_transaction_pool::BasicPool::new_full(
         config.transaction_pool.clone(),
+        config.role.is_authority().into(),
         config.prometheus_registry(),
         task_manager.spawn_handle(),
         client.clone(),
