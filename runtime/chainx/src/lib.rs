f0fd87c69f00a48a91240fb498f9b08839f67c03
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-02-22 11:34:24 +0800

    Add Proxy module (#478)
    * Add Proxy module
    
    * Cargo fmt
    
    * Bump spec version

--- runtime/chainx/src/lib.rs
+++ runtime/chainx/src/lib.rs
@@ -10,7 +10,7 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-use codec::Encode;
+use codec::{Decode, Encode};
 use static_assertions::const_assert;
 
 use sp_api::impl_runtime_apis;
@@ -31,7 +31,7 @@ use sp_runtime::{
         InvalidTransaction, TransactionPriority, TransactionSource, TransactionValidity,
         TransactionValidityError, ValidTransaction,
     },
-    ApplyExtrinsicResult, DispatchError, ModuleId, Perbill, Percent, Permill,
+    ApplyExtrinsicResult, DispatchError, ModuleId, Perbill, Percent, Permill, RuntimeDebug,
 };
 use sp_std::{collections::btree_map::BTreeMap, prelude::*};
 #[cfg(feature = "std")]
@@ -104,7 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-net"),
     authoring_version: 1,
-    spec_version: 9,
+    spec_version: 10,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
@@ -758,6 +758,115 @@ impl pallet_identity::Trait for Runtime {
     type WeightInfo = pallet_identity::weights::SubstrateWeight<Runtime>;
 }
 
+parameter_types! {
+    // One storage item; key size 32, value size 8; .
+    pub const ProxyDepositBase: Balance = deposit(1, 8);
+    // Additional storage item size of 33 bytes.
+    pub const ProxyDepositFactor: Balance = deposit(0, 33);
+    pub const MaxProxies: u16 = 32;
+    pub const AnnouncementDepositBase: Balance = deposit(1, 8);
+    pub const AnnouncementDepositFactor: Balance = deposit(0, 66);
+    pub const MaxPending: u16 = 32;
+}
+
+/// The type used to represent the kinds of proxying allowed.
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Encode, Decode, RuntimeDebug)]
+pub enum ProxyType {
+    Any = 0,
+    NonTransfer = 1,
+    Governance = 2,
+    Staking = 3,
+    IdentityJudgement = 4,
+    CancelProxy = 5,
+}
+
+impl Default for ProxyType {
+    fn default() -> Self {
+        Self::Any
+    }
+}
+
+impl InstanceFilter<Call> for ProxyType {
+    fn filter(&self, c: &Call) -> bool {
+        match self {
+            ProxyType::Any => true,
+            ProxyType::NonTransfer => matches!(
+                c,
+                Call::System(..)
+                    | Call::Scheduler(..)
+                    | Call::Babe(..)
+                    | Call::Timestamp(..)
+                    | Call::Indices(pallet_indices::Call::claim(..))
+                    | Call::Indices(pallet_indices::Call::free(..))
+                    | Call::Indices(pallet_indices::Call::freeze(..))
+                    // Specifically omitting Indices `transfer`, `force_transfer`
+                    // Specifically omitting the entire Balances pallet
+                    | Call::Authorship(..)
+                    | Call::XStaking(..)
+                    | Call::Offences(..)
+                    | Call::Session(..)
+                    | Call::Grandpa(..)
+                    | Call::ImOnline(..)
+                    | Call::AuthorityDiscovery(..)
+                    | Call::Democracy(..)
+                    | Call::Council(..)
+                    | Call::TechnicalCommittee(..)
+                    | Call::Elections(..)
+                    | Call::TechnicalMembership(..)
+                    | Call::Treasury(..)
+                    | Call::Utility(..)
+                    | Call::Identity(..)
+                    | Call::Proxy(..)
+                    | Call::Multisig(..)
+            ),
+            ProxyType::Governance => matches!(
+                c,
+                Call::Democracy(..)
+                    | Call::Council(..)
+                    | Call::TechnicalCommittee(..)
+                    | Call::Elections(..)
+                    | Call::Treasury(..)
+                    | Call::Utility(..)
+            ),
+            ProxyType::Staking => matches!(
+                c,
+                Call::XStaking(..) | Call::Session(..) | Call::Utility(..)
+            ),
+            ProxyType::IdentityJudgement => matches!(
+                c,
+                Call::Identity(pallet_identity::Call::provide_judgement(..)) | Call::Utility(..)
+            ),
+            ProxyType::CancelProxy => {
+                matches!(c, Call::Proxy(pallet_proxy::Call::reject_announcement(..)))
+            }
+        }
+    }
+    fn is_superset(&self, o: &Self) -> bool {
+        match (self, o) {
+            (x, y) if x == y => true,
+            (ProxyType::Any, _) => true,
+            (_, ProxyType::Any) => false,
+            (ProxyType::NonTransfer, _) => true,
+            _ => false,
+        }
+    }
+}
+
+impl pallet_proxy::Trait for Runtime {
+    type Event = Event;
+    type Call = Call;
+    type Currency = Balances;
+    type ProxyType = ProxyType;
+    type ProxyDepositBase = ProxyDepositBase;
+    type ProxyDepositFactor = ProxyDepositFactor;
+    type MaxProxies = MaxProxies;
+    type WeightInfo = pallet_proxy::weights::SubstrateWeight<Runtime>;
+    type MaxPending = MaxPending;
+    type CallHasher = BlakeTwo256;
+    type AnnouncementDepositBase = AnnouncementDepositBase;
+    type AnnouncementDepositFactor = AnnouncementDepositFactor;
+}
+
 ///////////////////////////////////////////
 // orml
 ///////////////////////////////////////////
@@ -956,6 +1065,8 @@ construct_runtime!(
         // It might be possible to merge this module into pallet_transaction_payment in future, thus
         // we put it at the end for keeping the extrinsic ordering.
         XTransactionFee: xpallet_transaction_fee::{Module, Event<T>} = 35,
+
+        Proxy: pallet_proxy::{Module, Call, Storage, Event<T>} = 37,
     }
 );
 
