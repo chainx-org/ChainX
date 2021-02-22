17af022fd9179f1e99e49e90ddcee8552c56a155
alannotnerd <alan1995wang@outlook.com>
2021-02-22 14:27:06 +0800

    Add test case for `execute_issue` (#484)
    * Add test case
    
    * Fix toml
    
    * Format code
    
    * Rename function
    
    * Use construct runtime
    
    * Update xpallets/gateway/bitcoin/v2/src/tests.rs
    
    Co-authored-by: Liu-Cheng Xu <xuliuchengxlc@gmail.com>
    
    * Update xpallets/gateway/bitcoin/v2/src/mock.rs
    
    Co-authored-by: Liu-Cheng Xu <xuliuchengxlc@gmail.com>
    
    Co-authored-by: Liu-Cheng Xu <xuliuchengxlc@gmail.com>

--- xpallets/gateway/bitcoin/v2/src/tests.rs
+++ xpallets/gateway/bitcoin/v2/src/tests.rs
@@ -1,17 +1,41 @@
+use light_bitcoin::chain::Transaction;
+
 use sp_arithmetic::Percent;
 
-use frame_support::{assert_err, assert_ok, dispatch::DispatchResultWithPostInfo};
-
-use crate::assets::types::TradingPrice;
+use frame_support::{
+    assert_err, assert_ok,
+    dispatch::{DispatchResult, DispatchResultWithPostInfo},
+};
+use frame_system::RawOrigin;
 
 use super::assets::pallet as assets;
 use super::issue::pallet as issue;
-use super::mock::{BuildConfig, ExtBuilder, Origin, Test};
 use super::redeem::pallet as redeem;
 use super::vault::pallet as vault;
 
-fn register_vault(id: u64, collateral: u128, addr: &str) -> DispatchResultWithPostInfo {
-    vault::Pallet::<Test>::register_vault(Origin::signed(id), collateral, addr.as_bytes().to_vec())
+use super::mock::*;
+
+fn t_register_vault(id: u64, collateral: u128, addr: &str) -> DispatchResultWithPostInfo {
+    Vault::register_vault(Origin::signed(id), collateral, addr.as_bytes().to_vec())
+}
+
+fn t_register_btc() -> DispatchResult {
+    type XAssetsRegistrar = xpallet_assets_registrar::Module<Test>;
+    type XAssets = xpallet_assets::Module<Test>;
+    let btc_asset = (
+        xp_protocol::X_BTC,
+        xpallet_assets_registrar::AssetInfo::new::<Test>(
+            b"X-BTC".to_vec(),
+            b"X-BTC".to_vec(),
+            xpallet_assets_registrar::Chain::Bitcoin,
+            8,
+            b"ChainX's cross-chain Bitcoin".to_vec(),
+        )
+        .unwrap(),
+        xpallet_assets::AssetRestrictions::empty(),
+    );
+    XAssetsRegistrar::register(RawOrigin::Root.into(), btc_asset.0, btc_asset.1, true, true)?;
+    XAssets::set_asset_limit(RawOrigin::Root.into(), btc_asset.0, btc_asset.2)
 }
 
 #[test]
@@ -22,20 +46,20 @@ fn test_register_vault() {
     })
     .execute_with(|| {
         assert_err!(
-            register_vault(1, 10000, "test"),
+            t_register_vault(1, 10000, "test"),
             assets::Error::<Test>::InsufficientFunds
         );
         assert_err!(
-            register_vault(1, 10, "test"),
+            t_register_vault(1, 10, "test"),
             vault::Error::<Test>::InsufficientVaultCollateralAmount
         );
-        assert_ok!(register_vault(1, 200, "test"));
+        assert_ok!(t_register_vault(1, 200, "test"));
         assert_err!(
-            register_vault(1, 200, "testuu"),
+            t_register_vault(1, 200, "testuu"),
             vault::Error::<Test>::VaultAlreadyRegistered
         );
         assert_err!(
-            register_vault(2, 200, "test"),
+            t_register_vault(2, 200, "test"),
             vault::Error::<Test>::BtcAddressOccupied
         );
     })
@@ -49,19 +73,16 @@ fn test_add_extra_collateral() {
     })
     .execute_with(|| {
         assert_err!(
-            vault::Pallet::<Test>::add_extra_collateral(Origin::signed(1), 100),
+            Vault::add_extra_collateral(Origin::signed(1), 100),
             vault::Error::<Test>::VaultNotFound
         );
-        assert_ok!(register_vault(1, 200, "test"));
+        assert_ok!(t_register_vault(1, 200, "test"));
         assert_err!(
-            vault::Pallet::<Test>::add_extra_collateral(Origin::signed(1), 10000),
+            Vault::add_extra_collateral(Origin::signed(1), 10000),
             assets::Error::<Test>::InsufficientFunds
         );
-        assert_ok!(vault::Pallet::<Test>::add_extra_collateral(
-            Origin::signed(1),
-            100
-        ));
-        let free_balance = pallet_balances::Module::<Test>::free_balance(1);
+        assert_ok!(Vault::add_extra_collateral(Origin::signed(1), 100));
+        let free_balance = Balances::free_balance(1);
         assert_eq!(free_balance, 700);
     })
 }
@@ -70,7 +91,7 @@ fn test_add_extra_collateral() {
 fn test_update_exchange_rate() {
     use super::assets::types::TradingPrice;
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        let exchange_rate = assets::Pallet::<Test>::exchange_rate();
+        let exchange_rate = Assets::exchange_rate();
         assert_eq!(exchange_rate.price, 0);
         assert_eq!(exchange_rate.decimal, 0);
 
@@ -80,21 +101,15 @@ fn test_update_exchange_rate() {
         };
 
         assert_err!(
-            assets::Pallet::<Test>::update_exchange_rate(
-                Origin::signed(2),
-                new_exchange_rate.clone()
-            ),
+            Assets::update_exchange_rate(Origin::signed(2), new_exchange_rate.clone()),
             assets::Error::<Test>::OperationForbidden
         );
-        assert_ok!(assets::Pallet::<Test>::force_update_oracles(
-            Origin::root(),
-            vec![0]
-        ));
-        assert_ok!(assets::Pallet::<Test>::update_exchange_rate(
+        assert_ok!(Assets::force_update_oracles(Origin::root(), vec![0]));
+        assert_ok!(Assets::update_exchange_rate(
             Origin::signed(0),
             new_exchange_rate.clone()
         ));
-        let exchange_rate = assets::Pallet::<Test>::exchange_rate();
+        let exchange_rate = Assets::exchange_rate();
         assert_eq!(exchange_rate, new_exchange_rate);
     })
 }
@@ -103,11 +118,10 @@ fn test_update_exchange_rate() {
 fn test_issue_request() {
     use super::assets::types::TradingPrice;
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        register_vault(1, 800, "test").unwrap();
-        issue::Pallet::<Test>::update_expired_time(Origin::root(), 10u64).unwrap();
-        issue::Pallet::<Test>::update_griefing_fee(Origin::root(), Percent::from_parts(10))
-            .unwrap();
-        assets::Pallet::<Test>::force_update_exchange_rate(
+        t_register_vault(1, 800, "test").unwrap();
+        Issue::update_expired_time(Origin::root(), 10u64).unwrap();
+        Issue::update_griefing_fee(Origin::root(), Percent::from_parts(10)).unwrap();
+        Assets::force_update_exchange_rate(
             Origin::root(),
             TradingPrice {
                 price: 1,
@@ -116,36 +130,46 @@ fn test_issue_request() {
         )
         .unwrap();
         assert_err!(
-            issue::Pallet::<Test>::request_issue(Origin::signed(2), 1, 1, 2),
+            Issue::request_issue(Origin::signed(2), 1, 1, 2),
             issue::Error::<Test>::InsufficientGriefingCollateral
         );
-        assert_ok!(issue::Pallet::<Test>::request_issue(
-            Origin::signed(2),
-            1,
-            1,
-            100
-        ));
+        assert_ok!(Issue::request_issue(Origin::signed(2), 1, 1, 100));
         let reserved_balance = <<Test as xpallet_assets::Config>::Currency>::reserved_balance(2);
         assert_eq!(reserved_balance, 100);
-        let issue_request = issue::Pallet::<Test>::get_issue_request_by_id(1).unwrap();
+        let issue_request = Issue::get_issue_request_by_id(1).unwrap();
         assert_eq!(issue_request.griefing_collateral, 100);
         assert_eq!(issue_request.requester, 2);
         assert_eq!(issue_request.vault, 1);
         assert_eq!(issue_request.open_time, 0);
 
         // check vault's token status
-        let vault = vault::Pallet::<Test>::get_vault_by_id(&issue_request.vault).unwrap();
+        let vault = Vault::get_vault_by_id(&issue_request.vault).unwrap();
         assert_eq!(vault.to_be_issued_tokens, issue_request.btc_amount);
+
+        t_register_btc().unwrap();
+
+        // execute issue_request
+        assert_ok!(Issue::execute_issue(
+            Origin::signed(1),
+            1,
+            vec![],
+            vec![],
+            Transaction::default(),
+        ));
+        let vault = Vault::get_vault_by_id(&issue_request.vault).unwrap();
+        assert_eq!(vault.issued_tokens, issue_request.btc_amount);
+        assert_eq!(vault.to_be_issued_tokens, 0);
     })
 }
 
+// Basic function test cases.
 #[test]
 fn test_lock_collateral() {
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        assert_ok!(assets::Pallet::<Test>::lock_collateral(&1, 200));
+        assert_ok!(Assets::lock_collateral(&1, 200));
         assert_eq!(<assets::CurrencyOf<Test>>::reserved_balance(1), 200);
         assert_err!(
-            assets::Pallet::<Test>::lock_collateral(&1, 1000),
+            Assets::lock_collateral(&1, 1000),
             assets::Error::<Test>::InsufficientFunds
         );
     });
@@ -154,12 +178,12 @@ fn test_lock_collateral() {
 #[test]
 fn test_slash_collateral() {
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        assets::Pallet::<Test>::lock_collateral(&1, 200).unwrap();
+        Assets::lock_collateral(&1, 200).unwrap();
         assert_err!(
-            assets::Pallet::<Test>::slash_collateral(&1, &2, 300),
+            Assets::slash_collateral(&1, &2, 300),
             assets::Error::<Test>::InsufficientCollateral
         );
-        assert_ok!(assets::Pallet::<Test>::slash_collateral(&1, &2, 200));
+        assert_ok!(Assets::slash_collateral(&1, &2, 200));
         assert_eq!(<assets::CurrencyOf<Test>>::reserved_balance(1), 0);
         assert_eq!(<assets::CurrencyOf<Test>>::reserved_balance(2), 200);
     });
@@ -168,9 +192,9 @@ fn test_slash_collateral() {
 #[test]
 fn test_release_collateral() {
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        assets::Pallet::<Test>::lock_collateral(&1, 200).unwrap();
+        Assets::lock_collateral(&1, 200).unwrap();
         assert_eq!(<assets::CurrencyOf<Test>>::reserved_balance(1), 200);
-        assert_ok!(assets::Pallet::<Test>::release_collateral(&1, 200));
+        assert_ok!(Assets::release_collateral(&1, 200));
         assert_eq!(<assets::CurrencyOf<Test>>::reserved_balance(1), 0);
         assert_err!(
             assets::Pallet::<Test>::release_collateral(&1, 200),
@@ -183,10 +207,9 @@ fn test_release_collateral() {
 fn test_redeem_request() {
     use super::assets::types::TradingPrice;
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        register_vault(1, 800, "test").unwrap();
-        issue::Pallet::<Test>::update_expired_time(Origin::root(), 10u64).unwrap();
-        issue::Pallet::<Test>::update_griefing_fee(Origin::root(), Percent::from_parts(10))
-            .unwrap();
+        t_register_vault(1, 800, "test").unwrap();
+        Issue::update_expired_time(Origin::root(), 10u64).unwrap();
+        Issue::update_griefing_fee(Origin::root(), Percent::from_parts(10)).unwrap();
         assets::Pallet::<Test>::force_update_exchange_rate(
             Origin::root(),
             TradingPrice {
@@ -232,10 +255,9 @@ fn test_redeem_request() {
 fn test_liquidation_redeem() {
     use super::assets::types::TradingPrice;
     ExtBuilder::build(BuildConfig::default()).execute_with(|| {
-        register_vault(1, 800, "test").unwrap();
-        issue::Pallet::<Test>::update_expired_time(Origin::root(), 10u64).unwrap();
-        issue::Pallet::<Test>::update_griefing_fee(Origin::root(), Percent::from_parts(10))
-            .unwrap();
+        t_register_vault(1, 800, "test").unwrap();
+        Issue::update_expired_time(Origin::root(), 10u64).unwrap();
+        Issue::update_griefing_fee(Origin::root(), Percent::from_parts(10)).unwrap();
         assets::Pallet::<Test>::force_update_exchange_rate(
             Origin::root(),
             TradingPrice {
@@ -245,12 +267,7 @@ fn test_liquidation_redeem() {
         )
         .unwrap();
 
-        assert_ok!(issue::Pallet::<Test>::request_issue(
-            Origin::signed(2),
-            1,
-            1,
-            100
-        ));
+        assert_ok!(Issue::request_issue(Origin::signed(2), 1, 1, 100));
 
         let reserved_balance = <<Test as xpallet_assets::Config>::Currency>::reserved_balance(2);
         assert_eq!(reserved_balance, 100);
