36ccc22abb1392760c7f8bb9245c897b0ea237fd
Alan WANG <alan1995wang@outlook.com>
2021-03-02 11:01:29 +0800

    Merge assets

--- xpallets/gateway/bitcoin/v2/src/lib.rs
+++ xpallets/gateway/bitcoin/v2/src/lib.rs
@@ -1,15 +1,349 @@
 // Copyright 2021 ChainX Project Authors. Licensed under GPL-3.0.
 
 //! This module implements Bitcoin Bridge V2.
+//!
+//! Bitcoin Bridge provides decentralized functionalities to manage digital assets between
+//! Bitcoin and ChainX.
+//!
+//! ## Terminology:
+//!
+//! *collateral*: PCX that reserved by bridge, which backs X-BTC.
+//!
+//! *vault*: Account that locks collateral in bridge, and is able to accept issue requesting by
+//! other accounts.
+//!
+//! *issue*: Operation that transfer BTC to a vault and issue equivalent X-BTC in ChainX.
+//!
+//! *redeem*: Opposite operation of `issue` that burn X-BTC and receive equivalent BTC in Bitcoin.
+//!
+//! *exchange rate oracle*: Role that updates exchange rate between BTC and PCX.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod assets;
 pub mod issue;
 pub mod redeem;
+pub(crate) mod types;
 pub mod vault;
 
 #[cfg(test)]
 mod mock;
 #[cfg(test)]
 mod tests;
+
+#[frame_support::pallet]
+#[allow(dead_code)]
+pub mod pallet {
+    use sp_arithmetic::traits::SaturatedConversion;
+    use sp_std::{marker::PhantomData, vec::Vec};
+
+    use frame_support::{
+        dispatch::{DispatchError, DispatchResult, DispatchResultWithPostInfo},
+        ensure,
+        storage::types::{StorageValue, ValueQuery},
+        traits::{Currency, Hooks, IsType, ReservableCurrency},
+    };
+    use frame_system::{
+        ensure_root, ensure_signed,
+        pallet_prelude::{BlockNumberFor, OriginFor},
+    };
+
+    use crate::types::{ErrorCode, Status, TradingPrice};
+
+    pub type BalanceOf<T> = <<T as xpallet_assets::Config>::Currency as Currency<
+        <T as frame_system::Config>::AccountId,
+    >>::Balance;
+
+    pub type CurrencyOf<T> = <T as xpallet_assets::Config>::Currency;
+
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(crate) trait Store)]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config + xpallet_assets::Config {
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
+
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+        fn on_initialize(n: BlockNumberFor<T>) -> frame_support::weights::Weight {
+            let height = Self::exchange_rate_update_time();
+            let period = Self::exchange_rate_expired_period();
+            if n - height > period {
+                <BridgeStatus<T>>::put(Status::Error(ErrorCode::EXCHANGE_RATE_EXPIRED));
+            };
+            0u64.into()
+        }
+
+        fn on_finalize(_: BlockNumberFor<T>) {
+            // recover from error if all errors were solved.
+            if let Status::Error(ErrorCode::NONE) = Self::bridge_status() {
+                <BridgeStatus<T>>::put(Status::Running);
+            }
+        }
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Update exchange rate by oracle.
+        #[pallet::weight(0)]
+        pub(crate) fn update_exchange_rate(
+            origin: OriginFor<T>,
+            exchange_rate: TradingPrice,
+        ) -> DispatchResultWithPostInfo {
+            let sender = ensure_signed(origin)?;
+            ensure!(Self::is_oracle(&sender), Error::<T>::OperationForbidden);
+            Self::_update_exchange_rate(exchange_rate.clone())?;
+            Self::deposit_event(Event::<T>::ExchangeRateUpdated(sender, exchange_rate));
+            Ok(().into())
+        }
+
+        /// Force update the exchange rate.
+        #[pallet::weight(0)]
+        pub(crate) fn force_update_exchange_rate(
+            origin: OriginFor<T>,
+            exchange_rate: TradingPrice,
+        ) -> DispatchResultWithPostInfo {
+            ensure_root(origin)?;
+            Self::_update_exchange_rate(exchange_rate.clone())?;
+            Self::deposit_event(Event::<T>::ExchangeRateForceUpdated(exchange_rate));
+            Ok(().into())
+        }
+
+        /// Force update the exchange rate expired period.
+        #[pallet::weight(0)]
+        pub(crate) fn force_update_exchange_rate_expired_period(
+            origin: OriginFor<T>,
+            expired_period: BlockNumberFor<T>,
+        ) -> DispatchResultWithPostInfo {
+            ensure_root(origin)?;
+            ExchangeRateExpiredPeriod::<T>::put(expired_period);
+            Self::deposit_event(Event::<T>::ExchangeRateExpiredPeriodForceUpdated(
+                expired_period,
+            ));
+            Ok(().into())
+        }
+
+        /// Force update oracles.
+        #[pallet::weight(0)]
+        pub(crate) fn force_update_oracles(
+            origin: OriginFor<T>,
+            oracles: Vec<T::AccountId>,
+        ) -> DispatchResultWithPostInfo {
+            ensure_root(origin)?;
+            OracleAccounts::<T>::put(oracles.clone());
+            Self::deposit_event(Event::<T>::OracleForceUpdated(oracles));
+            Ok(().into())
+        }
+    }
+
+    /// Events in xbridge module
+    ///
+    /// Emit when extrinsics or some important operators, like releasing/locking collateral,
+    /// move/transfer balance, etc, have happened.
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(crate) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Update exchange rate by oracle
+        ExchangeRateUpdated(T::AccountId, TradingPrice),
+        /// Update exchange rate by root
+        ExchangeRateForceUpdated(TradingPrice),
+        /// Update oracles by root
+        OracleForceUpdated(Vec<T::AccountId>),
+        /// Collateral was slashed. [from, to, amount]
+        CollateralSlashed(T::AccountId, T::AccountId, BalanceOf<T>),
+        // The collateral was released to the user successfully. [who, amount]
+        CollateralReleased(T::AccountId, BalanceOf<T>),
+        // Update `ExchangeRateExpiredPeriod`
+        ExchangeRateExpiredPeriodForceUpdated(BlockNumberFor<T>),
+    }
+
+    /// Errors for assets module
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Permission denied.
+        OperationForbidden,
+        /// Requester doesn't have enough pcx for collateral.
+        InsufficientFunds,
+        /// Arithmetic underflow/overflow.
+        ArithmeticError,
+        /// Account doesn't have enough collateral to be slashed.
+        InsufficientCollateral,
+        /// Bridge was shutdown or in error.
+        BridgeNotRunning,
+        /// Try to calculate collateral ratio while has no issued_tokens
+        NoIssuedTokens,
+    }
+
+    /// Total collateral locked by xbridge.
+    #[pallet::storage]
+    #[pallet::getter(fn total_collateral)]
+    pub(crate) type TotalCollateral<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+    /// Exchange rate from pcx to btc.
+    #[pallet::storage]
+    #[pallet::getter(fn exchange_rate)]
+    pub(crate) type ExchangeRate<T: Config> = StorageValue<_, TradingPrice, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn oracle_accounts)]
+    pub(crate) type OracleAccounts<T: Config> = StorageValue<_, Vec<T::AccountId>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn bridge_status)]
+    pub(crate) type BridgeStatus<T: Config> = StorageValue<_, Status, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn exchange_rate_update_time)]
+    pub(crate) type ExchangeRateUpdateTime<T: Config> =
+        StorageValue<_, BlockNumberFor<T>, ValueQuery>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn exchange_rate_expired_period)]
+    pub(crate) type ExchangeRateExpiredPeriod<T: Config> =
+        StorageValue<_, BlockNumberFor<T>, ValueQuery>;
+
+    impl<T: Config> Pallet<T> {
+        pub fn convert_to_pcx(btc_amount: BalanceOf<T>) -> Result<BalanceOf<T>, DispatchError> {
+            //TODO(wangyafei): add lower bound?
+            let exchange_rate = Self::exchange_rate();
+            let result = exchange_rate
+                .convert_to_pcx(btc_amount.saturated_into())
+                .ok_or(Error::<T>::ArithmeticError)?;
+            Ok(result.saturated_into())
+        }
+        /// Lock collateral
+        #[inline]
+        pub fn lock_collateral(sender: &T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
+            <<T as xpallet_assets::Config>::Currency as ReservableCurrency<
+                <T as frame_system::Config>::AccountId,
+            >>::reserve(sender, amount)
+            .map_err(|_| Error::<T>::InsufficientFunds)?;
+            <TotalCollateral<T>>::mutate(|total| *total += amount);
+            Ok(())
+        }
+
+        /// increase total collateral
+        #[inline]
+        pub fn increase_total_collateral(amount: BalanceOf<T>) {
+            <TotalCollateral<T>>::mutate(|c| *c += amount);
+        }
+
+        #[inline]
+        pub(crate) fn is_oracle(account: &T::AccountId) -> bool {
+            let oracles: Vec<T::AccountId> = Self::oracle_accounts();
+            oracles.contains(account)
+        }
+
+        pub(crate) fn _update_exchange_rate(exchange_rate: TradingPrice) -> DispatchResult {
+            // TODO: sanity check?
+            <ExchangeRate<T>>::put(exchange_rate);
+            let height = <frame_system::Pallet<T>>::block_number();
+            <ExchangeRateUpdateTime<T>>::put(height);
+            Self::recover_from_exchange_rate_expired();
+            Ok(())
+        }
+
+        /// Slash collateral to receiver
+        pub fn slash_collateral(
+            sender: &T::AccountId,
+            receiver: &T::AccountId,
+            amount: BalanceOf<T>,
+        ) -> DispatchResult {
+            let reserved_collateral = <CurrencyOf<T>>::reserved_balance(sender);
+            ensure!(
+                reserved_collateral >= amount,
+                Error::<T>::InsufficientCollateral
+            );
+            let (slashed, _) = <CurrencyOf<T>>::slash_reserved(sender, amount);
+
+            <CurrencyOf<T>>::resolve_creating(receiver, slashed);
+            <CurrencyOf<T>>::reserve(receiver, amount)
+                .map_err(|_| Error::<T>::InsufficientFunds)?;
+            Self::deposit_event(Event::<T>::CollateralSlashed(
+                sender.clone(),
+                receiver.clone(),
+                amount,
+            ));
+            Ok(().into())
+        }
+
+        /// Release collateral
+        pub fn release_collateral(account: &T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
+            let reserved_collateral = <CurrencyOf<T>>::reserved_balance(account);
+            ensure!(
+                reserved_collateral >= amount,
+                Error::<T>::InsufficientCollateral
+            );
+            <CurrencyOf<T>>::unreserve(account, amount);
+            <TotalCollateral<T>>::mutate(|total| *total -= amount);
+            Self::deposit_event(Event::<T>::CollateralReleased(account.clone(), amount));
+            Ok(())
+        }
+
+        /// Get if the bridge running
+        pub fn is_bridge_running() -> bool {
+            Self::bridge_status() == Status::Running
+        }
+
+        pub fn calculate_collateral_ratio(
+            issued_tokens: BalanceOf<T>,
+            collateral: BalanceOf<T>,
+        ) -> Result<u16, DispatchError> {
+            let issued_tokens = issued_tokens.saturated_into::<u128>();
+            let collateral = collateral.saturated_into::<u128>();
+            ensure!(issued_tokens != 0, Error::<T>::NoIssuedTokens);
+
+            let exchange_rate: TradingPrice = Self::exchange_rate();
+            let equivalence_collateral = exchange_rate
+                .convert_to_pcx(issued_tokens)
+                .ok_or(Error::<T>::ArithmeticError)?;
+            let raw_collateral: u128 = collateral.saturated_into();
+            let collateral_ratio = raw_collateral
+                .checked_mul(100)
+                .ok_or(Error::<T>::ArithmeticError)?
+                .checked_div(equivalence_collateral)
+                .ok_or(Error::<T>::ArithmeticError)?;
+            //FIXME(wangyafei): should use try_into?
+            Ok(collateral_ratio as u16)
+        }
+
+        #[inline]
+        pub(crate) fn ensure_bridge_running() -> DispatchResult {
+            ensure!(
+                Self::bridge_status() == Status::Running,
+                Error::<T>::BridgeNotRunning
+            );
+            Ok(())
+        }
+
+        /// Clarify `ExchangeRateExpired` is solved and recover from this error.
+        ///
+        /// Dangerous! Ensure this error truly solved is caller's responsibility.
+        pub(crate) fn recover_from_exchange_rate_expired() {
+            if let Status::Error(mut error_codes) = Self::bridge_status() {
+                if error_codes.contains(ErrorCode::EXCHANGE_RATE_EXPIRED) {
+                    error_codes.remove(ErrorCode::EXCHANGE_RATE_EXPIRED);
+                    <BridgeStatus<T>>::put(Status::Error(error_codes))
+                }
+            }
+        }
+
+        /// Clarify `Liquidating` is solved and recover from this error.
+        ///
+        /// Dangerous! Ensure this error truly solved is caller's responsibility.
+        pub(crate) fn recover_from_liquidating() {
+            if let Status::Error(mut error_codes) = Self::bridge_status() {
+                if error_codes.contains(ErrorCode::LIQUIDATING) {
+                    error_codes.remove(ErrorCode::LIQUIDATING);
+                    <BridgeStatus<T>>::put(Status::Error(error_codes))
+                }
+            }
+        }
+
+        pub(crate) fn reserved_balance_of(who: &T::AccountId) -> BalanceOf<T> {
+            CurrencyOf::<T>::reserved_balance(who)
+        }
+    }
+}
