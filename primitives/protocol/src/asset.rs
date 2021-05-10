ad8c6cde2236675942079c6a7f069be9909dc2d1
Alan WANG <alan1995wang@outlook.com>
2021-05-10 15:51:37 +0800

    Remove needless files

--- primitives/protocol/src/asset.rs
+++ primitives/protocol/src/asset.rs
@@ -49,6 +49,13 @@ pub const C_BTC: AssetId = 0xc0000000 | X_BTC;
 /// Shadow token for C_BTC
 pub const S_BTC: AssetId = 0xa0000000 | X_BTC;
 
+/// DOGE asset in ChainX backed by the Mainnet Dogecoin.
+pub const X_DOGE: AssetId = 3;
+/// Collateral based bitcoin in ChainX
+pub const C_DOGE: AssetId = 0xc0000000 | X_DOGE;
+/// Shadow token for C_BTC
+pub const S_DOGE: AssetId = 0xa0000000 | X_DOGE;
+
 /// ETH asset in ChainX backed by the Mainnet Ethereum.
 pub const X_ETH: AssetId = 60;
 
