3f710698be240e738e88c543c76924b225013ef2
AAweidai <1253464613@qq.com>
2021-06-01 11:06:21 +0800

    Correct const CHAINS

--- primitives/assets-registrar/src/lib.rs
+++ primitives/assets-registrar/src/lib.rs
@@ -14,12 +14,13 @@ use sp_std::slice::Iter;
 
 use chainx_primitives::AssetId;
 
-const CHAINS: [Chain; 5] = [
+const CHAINS: [Chain; 6] = [
     Chain::ChainX,
     Chain::Bitcoin,
     Chain::Ethereum,
     Chain::Polkadot,
     Chain::Dogecoin,
+    Chain::Binance,
 ];
 
 /// The blockchain types.
