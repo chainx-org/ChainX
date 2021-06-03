970dac688344f70d4746931effcd9d9715aa3662
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-06-03 10:06:11 +0800

    Complete run_wasm_runtime.sh

--- scripts/build_wasm_runtime.sh
+++ scripts/build_wasm_runtime.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# Make sure the script is running from the root directory of ChainX.
+cd "$(dirname "${BASH_SOURCE[0]}")"
+cd ..
+
 RUSTC_VERSION=nightly-2021-03-01
 PACKAGE=chainx-runtime
 
-# Use the toolchain specified in chainxorg/srtool
+# Use the toolchain specified in chainxorg/srtool instead of the one in ChainX
 if [ -f rust-toolchain ]; then
   mv rust-toolchain rust-toolchain.bak
 fi
