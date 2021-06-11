c95c6a8f47a03903a45cae9b0fdd2fc35ef69340
VegeBun-csj <csj1071285741@gmail.com>
2021-06-12 00:10:00 +0800

    migrate xpallets-assets-registrar pallet v2

--- xpallets/gateway/records/src/mock.rs
+++ xpallets/gateway/records/src/mock.rs
@@ -31,7 +31,7 @@ frame_support::construct_runtime!(
     {
         System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
         Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-        XAssetsRegistrar: xpallet_assets_registrar::{Pallet, Call, Storage, Event, Config},
+        XAssetsRegistrar: xpallet_assets_registrar::{Pallet, Call, Storage, Event<T>, Config},
         XAssets: xpallet_assets::{Pallet, Call, Storage, Event<T>, Config<T>},
         XGatewayRecords: xpallet_gateway_records::{Pallet, Call, Storage, Event<T>},
     }
