030cc0c82f379b2668585ce7bfbc7612439c37ec
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-06-11 11:18:54 +0800

    Update xpallet-gateway-bitcoin

--- xpallets/gateway/bitcoin/v1/src/tests/trustee.rs
+++ xpallets/gateway/bitcoin/v1/src/tests/trustee.rs
@@ -1,6 +1,6 @@
 // Copyright 2019-2020 ChainX Project Authors. Licensed under GPL-3.0.
 
-use frame_support::{assert_noop, assert_ok, storage::StorageValue};
+use frame_support::{assert_noop, assert_ok};
 use frame_system::RawOrigin;
 use hex_literal::hex;
 
