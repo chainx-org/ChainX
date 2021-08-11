542cb6e84df9d6e21a35e73e01a6171d5df5da41
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-08-11 22:49:25 +0800

    Regenesis (#574)
    * Place the deprecated modules
    
    * Use regenesis_params
    
    * Imple regenesis builder
    
    * Try loading the regenesis data
    
    * Commit regenesis data
    
    * Fix format
    
    * Use 3696796
    
    * Delete unused file
    
    * Remove deprecated_builder
    
    * Nits

---
+++ primitives/genesis-builder/src/regenesis_params.rs
@@ -0,0 +1,40 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct FreeBalanceInfo<AccountId, Balance> {
+    pub free: Balance,
+    pub who: AccountId,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Nomination<AccountId, Balance> {
+    pub nominee: AccountId,
+    pub nomination: Balance,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct NominatorInfo<AccountId, Balance> {
+    pub nominator: AccountId,
+    pub nominations: Vec<Nomination<AccountId, Balance>>,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct ValidatorInfo<AccountId, Balance> {
+    pub who: AccountId,
+    #[serde(with = "xp_rpc::serde_text")]
+    pub referral_id: Vec<u8>,
+    pub total_nomination: Balance,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct XStakingParams<AccountId, Balance> {
+    pub validators: Vec<ValidatorInfo<AccountId, Balance>>,
+    pub nominators: Vec<NominatorInfo<AccountId, Balance>>,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct AllParams<AccountId, Balance, AssetBalanceOf, StakingBalanceOf> {
+    pub balances: Vec<FreeBalanceInfo<AccountId, Balance>>,
+    pub xassets: Vec<FreeBalanceInfo<AccountId, AssetBalanceOf>>,
+    pub xstaking: XStakingParams<AccountId, StakingBalanceOf>,
+}
