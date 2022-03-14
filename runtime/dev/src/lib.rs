cd3e4a62d2e59bbda778be1723978bcfa8488bf1
AAweidai <dw1253464613@gmail.com>
2022-03-14 10:47:35 +0800

    Bump spec_version into 14 and version into 4.2.0

--- runtime/dev/src/lib.rs
+++ runtime/dev/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-dev"),
     authoring_version: 1,
-    spec_version: 13,
+    spec_version: 14,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 4,
