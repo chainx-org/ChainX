e5d2f83438ea71c9dfff47d166a4beadad4c03bf
icodezjb <icodezjb@users.noreply.github.com>
2022-03-15 11:23:49 +0800

    Use runtime version `16`

--- runtime/chainx/src/lib.rs
+++ runtime/chainx/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-net"),
     authoring_version: 1,
-    spec_version: 14,
+    spec_version: 16,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 4,
