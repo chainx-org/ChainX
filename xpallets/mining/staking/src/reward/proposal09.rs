d0f0c335e88afaced8182963f8ff7092cd1d950e
icodezjb <icodezjb@users.noreply.github.com>
2022-03-17 17:45:05 +0800

    Fix distribute_to_active_validators

--- xpallets/mining/staking/src/reward/proposal09.rs
+++ xpallets/mining/staking/src/reward/proposal09.rs
@@ -76,7 +76,16 @@ impl<T: Config> Pallet<T> {
     fn distribute_to_active_validators(
         session_reward: BalanceOf<T>,
     ) -> Vec<(T::AccountId, BalanceOf<T>)> {
-        let current_validators = T::SessionInterface::validators();
+        let current_validators: Vec<(T::AccountId, BalanceOf<T>)> =
+            T::SessionInterface::validators()
+                .into_iter()
+                .filter(|v| Self::is_active(v))
+                .map(|v| {
+                    let total_votes = Self::total_votes_of(&v);
+                    (v, total_votes)
+                })
+                .collect();
+        
         let mut total_stake = current_validators
             .iter()
             .fold(Zero::zero(), |acc: BalanceOf<T>, (_, x)| acc + *x);
