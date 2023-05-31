61a48062e0c64569236357e80fc17d8d45a3cffd
icodezjb <icodezjb@users.noreply.github.com>
2023-05-31 14:44:15 +0800

    Update chainx-malan runtime version to 29

--- runtime/malan/src/lib.rs
+++ runtime/malan/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-malan"),
     authoring_version: 1,
-    spec_version: 28,
+    spec_version: 29,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 6,
