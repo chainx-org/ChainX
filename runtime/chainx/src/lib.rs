79a3f9c72c6fefdd4949ed2889d733d2559a1d82
icodezjb <icodezjb@users.noreply.github.com>
2024-02-04 19:11:08 +0800

    Bump runtime version 33 -> 34

--- runtime/chainx/src/lib.rs
+++ runtime/chainx/src/lib.rs
@@ -125,7 +125,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("chainx"),
     impl_name: create_runtime_str!("chainx-net"),
     authoring_version: 1,
-    spec_version: 33,
+    spec_version: 34,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 7,
